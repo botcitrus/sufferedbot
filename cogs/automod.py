9ccc789529ebbaa9f201c1fd77ea62efe611c310
Andrew <avmosc240206@gmail.com>
2022-05-27 22:52:19 +0900

    creating repo

--- cogs/automod.py
+++ cogs/automod.py
@@ -8,10 +8,9 @@ class automod(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if message:
-            return
-# soon
+    async def on_member_join(self, member):
+        role = self.bot.get_role(975421175582826496)
+        await member.add_role(role)
 
 
 def setup(bot):
