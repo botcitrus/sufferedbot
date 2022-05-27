0d5f24d9643108b9837427e407872f81a9c95a94
Andrew <avmosc240206@gmail.com>
2022-05-27 22:18:03 +0900

    creating repo

--- cogs/logs.py
+++ cogs/logs.py
@@ -9,24 +9,30 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        guild = message.guild
-        channel = guild.get_channel(979047685912928317)
-        embed = discord.Embed(color=0x2f3136, timestamp=datetime.datetime.utcnow())
-        embed.add_field(name="Удаленное сообщение:", value=message.content)
-        embed.add_field(name="Чат", value=message.channel.mention, inline=False)
-        embed.set_author(name=f"{message.author.name} удалил сообщение", icon_url=message.author.avatar.url)
-        await channel.send(embed=embed)
+        if message.author.id == 975327611267981314:
+            return
+        else:
+            guild = message.guild
+            channel = guild.get_channel(979047685912928317)
+            embed = discord.Embed(color=0x2f3136, timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Удаленное сообщение:", value=message.content)
+            embed.add_field(name="Чат", value=message.channel.mention, inline=False)
+            embed.set_author(name=f"{message.author.name} удалил сообщение", icon_url=message.author.avatar.url)
+            await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        guild = before.guild
-        channel = guild.get_channel(979047685912928317)
-        embed = discord.Embed(color=0x2f3136, timestamp=datetime.datetime.utcnow())
-        embed.add_field(name="До:", value=before.content, inline=True)
-        embed.add_field(name="После:", value=after.content, inline=True)
-        embed.add_field(name="Чат", value=before.channel.mention, inline=False)
-        embed.set_author(name=f"{before.author.name} изменил сообщение", icon_url=before.author.avatar.url)
-        await channel.send(embed=embed)
+        if before.author.id == 975327611267981314:
+            return
+        else:
+            guild = before.guild
+            channel = guild.get_channel(979047685912928317)
+            embed = discord.Embed(color=0x2f3136, timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="До:", value=before.content, inline=True)
+            embed.add_field(name="После:", value=after.content, inline=True)
+            embed.add_field(name="Чат", value=before.channel.mention, inline=False)
+            embed.set_author(name=f"{before.author.name} изменил сообщение", icon_url=before.author.avatar.url)
+            await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,12 +8,15 @@ class moderation(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.has_role(roles['Admin'], roles['Moderator'])
-    async def clear(self, ctx, amount):
-        await ctx.channel.purge(limit=amount)
-        msg = await ctx.send("*Сообщения удалены...*")
-        await asyncio.sleep(2)
-        await msg.delete()
+    @commands.has_any_role(roles['Admin'], roles['Moderator'])
+    async def clear(self, ctx, amount: int):
+        if not amount:
+            await ctx.send("Вы не указали кол-во сообщений")
+        else:
+            await ctx.channel.purge(limit=amount)
+            msg = await ctx.send("*Сообщения удалены...*")
+            await asyncio.sleep(2)
+            await msg.delete()
 
 
 def setup(bot):
