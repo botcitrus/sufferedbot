4088c1cbfebc086553527704644bf51786dbece6
Andrew <avmosc240206@gmail.com>
2022-05-30 17:45:09 +0900

    creating repo

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,7 @@
 import asyncio
+
+import discord
+
 from config import roles
 from discord.ext import commands
 
@@ -17,6 +20,27 @@ class moderation(commands.Cog):
             msg = await ctx.send("*Сообщения удалены...*")
             await asyncio.sleep(2)
             await msg.delete()
+
+    @commands.command()
+    @commands.has_any_role(roles['Admin'], roles['Moderator'])
+    async def ban(self, ctx, member, *, reason):
+        if not member:
+            embed1 = discord.Embed(title="Неправильное использование команды", color=0xff4d4d)
+            embed1.set_author(name="Ошибка",
+                             icon_url="https://cdn.discordapp.com/attachments/976450230197305435/976450304478416896"
+                                      "/chat.png")
+            embed1.add_field(name="Пример:", value="`!ban <@пользователь> <причина>`", inline=False)
+            msg = await ctx.send(embed=embed1)
+            await asyncio.sleep(2)
+            await msg.delete()
+        else:
+            embed = discord.Embed(color=0xffee33)
+            embed.set_author(name=f"{ctx.author.name} забанил пользователя", icon_url=ctx.author.avatar.url)
+            embed.add_field(name="Пользователь:", value=member.name, inline=True)
+            embed.add_field(name="Причина:", value=reason, inline=True)
+            await member.ban(reason=reason)
+            await ctx.send(embed=embed)
+
 
 
 def setup(bot):
